cdc49aa529c0102fdffb744671132d1de279fdbb
Abhishek <abhishekmdp11@gmail.com>
2025-01-29 23:26:27 +0530

    Fixed hover effect

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -78,7 +78,7 @@ const About = ({ isDarkMode }) => {
           >
             {infoList.map(({ icon, iconDark, title, description }, index) => (
               <motion.li
-                whileHover={{ scale: 1 }}
+                whileHover={{ scale: 1.05 }}
                 className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50"
                 key={index}
               >
@@ -114,7 +114,7 @@ const About = ({ isDarkMode }) => {
           >
             {toolsData.map((tool, index) => (
               <motion.li
-                whileHover={{ scale: 1 }}
+                whileHover={{ scale: 1.05 }}
                 className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
                 key={index}
               >
